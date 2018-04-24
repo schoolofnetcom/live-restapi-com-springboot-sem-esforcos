c91457885f76845adc0baed50de526b85d1d9ee6
Leonan Luppi <leonan.luppi@gmail.com>
2018-04-24 09:41:14 -0300

    init

---
+++ src/main/java/com/schoolofnet/liveRestAPI/LiveRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.schoolofnet.liveRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveRestApiApplication.class, args);
+	}
+}
